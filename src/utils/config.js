af878d4439f3605517093dcb587766c146921469
Heberth López <102813488+heblopez@users.noreply.github.com>
2024-08-18 12:40:22 -0500

    Updated config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 export const API_TOKEN_KEY = "eatable-token";
-export const API_BASE_URI = "https://react-eatable-api.herokuapp.com";
+export const API_BASE_URI = "https://eatable-api-production.up.railway.app";
